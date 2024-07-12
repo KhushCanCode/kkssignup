d440b13831302cbecdc9010323ce95146dc49c8a
Shubham Dave <107801015+shaayar@users.noreply.github.com>
2024-07-13 01:40:59 +0530

    added footer

--- src/components/Footer/page.jsx
+++ src/components/Footer/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-white text-black w-screen py-[1vw] px-[2vw] text-center'>
+        <h1>Copyright © 2024 – Kabad Korner Cooperative.</h1>
+    </footer>
   )
 }
 
--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -6,10 +6,12 @@ const Header = () => {
     { path: '/', text: 'Home' },
     { path: '/about', text: 'About' },
     { path: '/contact', text: 'Contact' },
+    { path: '/login', text: 'Log In'},
+    { path: '/signup', text: 'Sign Up'}
     // Add more routes as needed
   ];
   return (
-    <nav className='sticky bg-white text-black flex flex-col gap-[1vw]'>
+    <nav className='sticky bg-white text-black flex flex-col'>
       <div className="flex justify-between px-[2vw] py-[1vw]">
         <h1>Kabadi Korner</h1>
         <div className="dets text-sm">
@@ -17,9 +19,9 @@ const Header = () => {
           <h1>+91 1234567890</h1>
         </div>
       </div>
-      <div className="links flex gap-[2vw] justify-end border-t-[.3vh] border-black py-[1vw] px-[2vw]">
+      <div className="links group flex gap-[2.4vw] justify-end border-t-[.3vh] border-black py-[1vw] px-[2vw] hover:h-[5vw] align-middle">
         {routes.map((route) => (
-          <Link key={route.path} href={route.path} passHref className='hover:underline underline-offset-4 decoration-2'>{route.text}</Link>
+          <Link key={route.path} href={route.path} passHref className='hover:underline underline-offset-4 decoration-2 hover:rounded-3xl hover:bg-gray-300 hover:py-[0.6vw] transition-all hover:px-[1.2vw] group-hover:pt-2'>{route.text}</Link>
         ))}
       </div>
     </nav>
