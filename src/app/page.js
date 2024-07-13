823d8b0f503ceed8256cc0d204fee08d8b802acd
Shubham Dave <107801015+shaayar@users.noreply.github.com>
2024-07-13 10:50:59 +0530

    added login and signup

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
+import Landing from "@/pages/Landing/page";
+import Login from "@/pages/Login/page";
+import SignUp from "@/pages/Signup/page";
 
 export default function Home() {
   return (
-    <main>Your components go here!!</main>
+    <main>
+      <Landing />
+    </main>
   );
 }
--- src/components/Footer/page.jsx
+++ src/components/Footer/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-white text-black w-screen py-[1vw] px-[2vw] text-center'>
+    <footer className='text-white bg-[#0c0c0c] w-screen py-[1vw] px-[2vw] text-center'>
         <h1>Copyright © 2024 – Kabad Korner Cooperative.</h1>
     </footer>
   )
